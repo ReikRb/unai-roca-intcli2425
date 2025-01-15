84495d64b41b68d946e34ff560ee98eca36634fd
Unai <reikrb@gmail.com>
2025-01-15 13:00:42 +0100

    adds level filter in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,63 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Potion } from './types/Potion'
+import { potions } from './data/data'
+import { filterByLevelRequirement } from './helpers/potionHelpers'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [potionsToDisplay, setPotionsToDisplay] = useState<Potion[]>(potions)
+  const [value, setValue] = useState(100);
+
+  const handleChange = (event: any) => {
+    const level = event.target.value
+    setValue(level)
+    const newPotions = filterByLevelRequirement(potions, level)
+    setPotionsToDisplay(newPotions)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className=' w-[900px] h-[500px] ml-[30%] flex flex-row space-x-[5%] overflow-y-auto'>
+      {
+        potionsToDisplay.map((potion: Potion, index: number) => {
+          return (
+            <div className=' w-[350px] h-[500px] mb-[7%] border-2 border-[#e5d774]'>
+              <div className='justify-items-center'>
+                <p className='mb-[7%]'>{potion.name}</p>
+                <img src={`./${potion.image}`} className='pl-[15%]'/>
+              </div>
+              <div className='flex flex-block mt-[5%] space-x-[10%]'>
+                <p>Rarity: {potion.rarity}</p>
+                <p>Boss: {potion.meta.availability.drop_rate.boss}</p>
+                <p>Drop Chance: {potion.meta.availability.drop_rate.chance}</p>
+              </div>
+              <div className='mt-[6%] mb-[6%]'>
+                <button>Details</button>
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+        <span>FILTERS {value}</span>
+    <div className=' flex w-[1800px] h-[300px] ml-[4%] mt-[4%]'>
+      <div className='flex flex-block w-[20%] mr-[5%]'>
+
+
+      <label htmlFor="minmax-range" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Min-max range</label>
+      <input id="minmax-range" type="range" min="0" max="100" defaultValue={value} 
+        onChange={handleChange}
+        className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='w-[20%] mr-[5%]'>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='w-[20%] mr-[5%]'>
+      </div>
+      <div className=' w-[20%] bg-white'>
+
+      </div>
+    </div>
     </>
   )
 }
