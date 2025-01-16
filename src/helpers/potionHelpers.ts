a113180215f86eb24fc233ee6c6096b6653415b0
Unai <reikrb@gmail.com>
2025-01-16 12:34:42 +0100

    updates helpers

--- src/helpers/potionHelpers.ts
+++ src/helpers/potionHelpers.ts
@@ -3,51 +3,33 @@ import { Potion } from "../types/Potion";
 import { SecondaryEffect } from "../types/SecondaryEffect";
 
 export function filterByLevelRequirement(potions: Potion[], level: number) {
-    let newPotions: Potion[] = []
-    potions.map((potion: Potion) => {
-        potion.usage.restrictions.levelRequirement <= level ? newPotions = [...newPotions, potion] : null
-    })
-    return newPotions
+    return potions.filter((potion: Potion) => potion.usage.restrictions.levelRequirement <= level)
 }
 
 export function getPotionsByRarity(potions: Potion[], rarity: string) {
-    let newPotions: Potion[] = []
-    potions.map((potion: Potion) => {
-        potion.rarity === rarity ? newPotions = [...newPotions, potion] : null
-    })
-    return newPotions
+    return potions.filter((potion: Potion) => potion.rarity === rarity)
 }
 
-export function listIngredients(potion: Potion) {
-    let ingredients: string[] = []
-    potion.ingredients.map((ingredient: Ingredient) => {
-        ingredients = [...ingredients, ingredient.name]
-    })
-    return ingredients
+export function listIngredients(potion: Potion): string[] {
+    return potion.ingredients.map((ingredient: Ingredient) => ingredient.name)
 }
 
-export function findPotionByEffect(potions: Potion[], effectName: string) {
-    let newPotions: Potion[] = []
-    potions.map((potion: Potion) => {
-        potion.effects.secondary.map((effect: SecondaryEffect) => {
-            effect.attribute === effectName ? newPotions = [...newPotions, potion] : null
-        })
-    })
-    return newPotions
+export function findPotionByEffect(potions: Potion[], effectName: string): Potion[] {
+    return potions.filter((potion: Potion) =>
+        potion.effects.secondary.some((effect: SecondaryEffect) => effect.attribute === effectName)
+    )
 }
 
 export function calculateCraftingTime(potions: Potion[]) {
-    let craftingTimeMinutes: number = 0
-    let craftingTimeHours: number = 0
+    let craftingTime: number = 0
     potions.map((potion: Potion) => {
         const unit: string = potion.crafting.time.unit
         const amount: number = potion.crafting.time.amount
 
-        unit === 'hours' ? craftingTimeHours += amount : craftingTimeMinutes += amount
+        unit === 'hours' ? craftingTime += amount * 60 
+                         : craftingTime += amount
     })
-    console.log('Minutes:', craftingTimeMinutes, 'Hours: ', craftingTimeHours);
-    craftingTimeMinutes += craftingTimeHours*60
 
-    console.log('TOTAL TIME TO CRAFT ALL POTIONS: ', craftingTimeMinutes)
-    return craftingTimeMinutes
+    console.log('TOTAL TIME TO CRAFT ALL POTIONS: ', craftingTime)
+    return craftingTime
 }
